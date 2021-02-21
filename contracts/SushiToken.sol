c28ac763d2c7c773435f5a77abf3e335d10c611a
cimerr1 <39150493+cimerr1@users.noreply.github.com>
2021-02-21 21:56:00 +0530

    Update SushiToken.sol

--- contracts/SushiToken.sol
+++ contracts/SushiToken.sol
@@ -2,12 +2,12 @@
 
 pragma solidity 0.6.12;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/BEP20/BEP20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-// SushiToken with Governance.
-contract SushiToken is ERC20("SushiToken", "SUSHI"), Ownable {
+// hKimchiToken with Governance.
+contract hKimchiToken is BEP20("hKimchiToken", "HKCHI"), Ownable {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
